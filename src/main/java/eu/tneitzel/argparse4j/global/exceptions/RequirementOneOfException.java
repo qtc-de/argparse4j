a2b76157a2051250c69e5ae105a69e6fd47bde43
Tobias Neitzel <tobias.neitzel@code-white.com>
2024-01-01 22:29:55 +0100

    Small formatting change

--- src/main/java/eu/tneitzel/argparse4j/global/exceptions/RequirementOneOfException.java
+++ src/main/java/eu/tneitzel/argparse4j/global/exceptions/RequirementOneOfException.java
@@ -8,10 +8,10 @@ import eu.tneitzel.argparse4j.global.IOption;
  */
 public class RequirementOneOfException extends RequirementException
 {
+    private static final long serialVersionUID = 796880845020916443L;
+
     public RequirementOneOfException(IOption[] options)
     {
         super(options);
     }
-
-    private static final long serialVersionUID = 796880845020916443L;
 }
